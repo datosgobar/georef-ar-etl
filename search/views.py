d5cbe049b38180acd111987e4de26b03363b2c48
Arturo Peroni <arperoni@gmail.com>
2017-03-10 10:06:12 -0300

    Renombra variables de los resultados HERE y OSM.

--- search/views.py
+++ search/views.py
@@ -3,14 +3,14 @@ from search.wrappers import HereWrapper, NominatimWrapper
 
 
 def search(request):
-    data = {}
+    data = {'results': {}}
     if request.method == 'POST':
         address = request.POST.get('address', True)
         here_wrapper = HereWrapper(
             url='https://geocoder.cit.api.here.com/6.2/geocode.json',
             app_code='2KEvGLCDtqH7HLUTNxDu3A&gen=8',
             app_id='Qop1chzvUyZdOZJatmeG')
-        data['here_data'] = here_wrapper.search_address(address)
+        data['results'].update(here=here_wrapper.search_address(address))
 
         nominatim_wrapper = NominatimWrapper(
             url='http://nominatim.openstreetmap.org/',
@@ -18,7 +18,7 @@ def search(request):
             polygon='1',
             address_details='1'
         )
-        data['osm_data'] = nominatim_wrapper.search_address(address)
+        data['results'].update(osm=nominatim_wrapper.search_address(address))
 
     return render(request, 'search.html', data)
 
