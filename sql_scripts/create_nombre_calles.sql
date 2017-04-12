6600025cb45a0bed58ed2323503015768a69e01d
Marisol Cardozo <cardozo.marisolp@gmail.com>
2017-04-12 16:17:59 -0300

    Agrega función que elimina duplicados de la tabla localidades

--- sql_scripts/create_nombre_calles.sql
+++ sql_scripts/create_nombre_calles.sql
@@ -1,4 +1,31 @@
-CREATE TABLE public.nombre_calles AS 
+--
+-- Función que elimina duplicados de la tabla postal.localidades
+-- Name: delete_duplicates(integer, integer, character varying); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION delete_duplicates(_id integer, _id_paraje integer, _nombre character varying) RETURNS void
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+	delete from postal.localidades where id = (
+		select id
+		from postal.localidades
+		where id != _id
+		and id_paraje = _id_paraje
+		and nombre = _nombre
+		and id > _id
+		order by nombre asc
+		limit 1
+
+	);
+END;
+$$;
+
+--
+-- Tabla nombre de calles
+--
+
+CREATE TABLE public.nombre_calles AS
 	SELECT pc.id, pc.nombre_completo, pc.nombre_abreviado, pc.apellido, pc.nombre, tp.nombre tipo_camino, pl.nombre localidad, pp.nombre partido, ppv.nombre provincia
 	FROM postal.calles pc INNER JOIN postal.localidades pl 
 	ON pc.id_localidad = pl.id INNER JOIN postal.tipos_camino tp 
