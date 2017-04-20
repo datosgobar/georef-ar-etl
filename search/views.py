bf2ed2f48e175fea24eff93e54806fb167f4a1d6
Arturo Peroni <arperoni@gmail.com>
2017-04-20 11:56:23 -0300

    Elimina código relacionado a HERE y OSM.

--- search/views.py
+++ search/views.py
@@ -1,5 +1,3 @@
-import json
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from search.wrappers import PostgresWrapper
 
@@ -12,20 +10,3 @@ def search(request):
         data['suggestions'] = wrapper.search_address(address)
 
     return render(request, 'search.html', data)
-
-
-def save_address(request):
-    if request.method == 'POST':
-        search_text = request.POST.get('search_text', True)
-        result = request.POST.get('result', True)
-        result = json.loads(result.replace('\'', '"'))
-        if result['source'] == 'osm':
-            wrapper = NominatimWrapper(
-                url=None, format=None, country_code=None, address_details=None)
-        else:
-            wrapper = HereWrapper(
-                url=None, app_code=None, app_id=None)
-        address = wrapper.get_address_from(search_text, result['address'])
-        address.save()
-        return HttpResponse('Se guardó la dirección correctamente.')
-    return redirect('search')
