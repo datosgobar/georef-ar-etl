ae066d89aca9f1da2d2b5f4e4ebd134e9f29d626
Arturo Peroni <arperoni@gmail.com>
2017-04-21 11:39:13 -0300

    Procesa búsqueda con comas para filtrar por ciudad.

--- search/wrappers.py
+++ search/wrappers.py
@@ -10,13 +10,19 @@ class PostgresWrapper:
     def search_address(self, address):
         connection = psycopg2.connect(
             dbname="georef", user="postgres", password="postgres")
+        parts = address.split(',')
         with connection.cursor() as cursor:
             query = "SELECT tipo_camino || ' ' || nombre_completo || ', ' \
-                    || localidad || ', ' || provincia AS addr \
-                    FROM nombre_calles \
-                    WHERE nombre_completo ILIKE '%(address)s%%' \
-                    OR localidad ILIKE '%(address)s%%' \
-                    LIMIT 10" % {'address': address}
+                        || localidad || ', ' || provincia AS addr \
+                        FROM nombre_calles "
+            if len(parts) > 1:
+                query += "WHERE nombre_completo ILIKE '%(road)s%%' \
+                        AND localidad ILIKE '%%%(locality)s%%'" % {
+                            'road': parts[0].strip(),
+                            'locality': parts[1].strip()}
+            else:
+                query += "WHERE nombre_completo ILIKE '%s%%'" % (address)
+            query += " LIMIT 10"
             cursor.execute(query)
             results = cursor.fetchall()
         
