df1b6bbe2923076b24c7fe6404ae4ed3acbd5792
Arturo Peroni <arperoni@gmail.com>
2017-05-16 11:55:44 -0300

    Página de comparación de servicios.

--- search/urls.py
+++ search/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.search, name='search'),
-    # url(r'^save_address', views.save_address, name='save_address'),
+    url(r'^comparar', views.compare, name='compare'),
+    url(r'^save_address', views.save_address, name='save_address'),
 ]
--- search/views.py
+++ search/views.py
@@ -1,12 +1,51 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from search.wrappers import APIWrapper
+from search.wrappers import GeorefWrapper, HereWrapper, NominatimWrapper
+import json
 
 
 def search(request):
     data = {'results': {}}
     if request.is_ajax():
         address = request.GET.get('term', True)
-        data = APIWrapper().search_address(address)
+        data = GeorefWrapper().search_address(address)
         return HttpResponse(data)
     return render(request, 'search.html', data)
+
+
+def compare(request):
+    data = {'results': {}}
+    if request.method == 'POST':
+        address = request.POST.get('address', True)
+        here_wrapper = HereWrapper(
+            url='https://geocoder.cit.api.here.com/6.2/geocode.json',
+            app_code='2KEvGLCDtqH7HLUTNxDu3A&gen=8',
+            app_id='Qop1chzvUyZdOZJatmeG')
+        data['results'].update(here=here_wrapper.search_address(address))
+
+        nominatim_wrapper = NominatimWrapper(
+            url='http://nominatim.openstreetmap.org/',
+            format='json',
+            country_code='ar',
+            address_details='1'
+        )
+        data['results'].update(osm=nominatim_wrapper.search_address(address))
+
+    return render(request, 'compare.html', data)
+
+
+def save_address(request):
+    if request.method == 'POST':
+        search_text = request.POST.get('search_text', True)
+        result = request.POST.get('result', True)
+        result = json.loads(result.replace('\'', '"'))
+        if result['source'] == 'osm':
+            wrapper = NominatimWrapper(
+                url=None, format=None, country_code=None, address_details=None)
+        else:
+            wrapper = HereWrapper(
+                url=None, app_code=None, app_id=None)
+        address = wrapper.get_address_from(search_text, result['address'])
+        address.save()
+        return HttpResponse('Se guardó la dirección correctamente.')
+    return redirect('compare')
