d3a8145537df27616579315fe71e8d2bf566a227
Marisol Cardozo <cardozo.marisolp@gmail.com>
2017-06-19 10:58:42 -0300

    Agrega la función que devuelve el centroide de una calle. closes #40

--- sql_scripts/san_luis_geocoder.sql
+++ sql_scripts/san_luis_geocoder.sql
@@ -1,6 +1,6 @@
 /*
  Función de geocodificación
- Parametros: Nombre de calle (text), Altura (integer)
+ Parámetros: Nombre de calle (text), Altura (integer)
  Retorna: Latitud, Logitud (text)
 */
 CREATE OR REPLACE FUNCTION geocodificar(IN calle TEXT, IN altura INTEGER, OUT result TEXT)
@@ -45,14 +45,13 @@ BEGIN
 END;
 $$ LANGUAGE 'plpgsql';
 
-
 -- Ejemplo de uso
 SELECT geocodificar('JUAN B JUSTO', 452);
 
 
 /*
   Función que devuelve el punto inicial de una calle
-  Parametros: Nombre de calle (text)
+  Parámetros: Nombre de calle (text)
   Retorna: Latitud, Logitud (text)
 */
 CREATE OR REPLACE FUNCTION get_punto_inicial(IN nombre TEXT, OUT result TEXT)
@@ -91,3 +90,33 @@ $$ LANGUAGE 'plpgsql';
 
 -- Ejemplo de uso
 SELECT get_punto_inicial('LOS ANDES');
+
+
+/*
+  Función que devuelve el centroide de una calle
+  Parámetros: Nombre de calle (text)
+  Retorna: Latitud, Logitud (text)
+*/
+CREATE OR REPLACE FUNCTION get_centroide(IN calle TEXT, OUT lat_lng TEXT)
+AS $$
+DECLARE
+  lat TEXT;
+  lng TEXT;
+BEGIN
+  IF (calle = '') IS NOT FALSE
+  THEN
+    RAISE EXCEPTION 'El nombre de la calle es requerido.';
+  ELSE
+    SELECT
+      st_y(st_centroid(st_collect(geom))),
+      st_x(st_centroid(st_collect(geom)))
+    INTO lat, lng
+    FROM san_luis.justo_daract_callejero
+    WHERE nombre ILIKE '%' || $1 || '%';
+    lat_lng := lat || ', ' || lng;
+  END IF;
+END;
+$$ LANGUAGE 'plpgsql';
+
+-- Ejemplo de uso
+SELECT get_centroide('JUAN B JUSTO');
