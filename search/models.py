ff47480b98b4ed01c3ce2eebaef65d5294ea3cb5
Arturo Peroni <arperoni@gmail.com>
2017-08-09 15:38:40 -0300

    Elimina la clase Search.

--- search/models.py
+++ search/models.py
@@ -52,20 +52,3 @@ class Address(models.Model):
 
     def __str__(self):
         return '{} {}'.format(self.name, self.house_number)
-
-
-class Search(models.Model):
-    text = models.CharField(max_length=250)
-    road_name = models.CharField(max_length=100, blank=True, null=True)
-    road_type = models.CharField(max_length=25, blank=True, null=True)
-    door_number = models.IntegerField(blank=True, null=True)
-    postal_code = models.CharField(max_length=8, blank=True, null=True)
-    locality = models.ForeignKey(City, blank=True, null=True)
-    state = models.ForeignKey(State, blank=True, null=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        verbose_name_plural = 'Searches'
-
-    def __str__(self):
-        return self.text
