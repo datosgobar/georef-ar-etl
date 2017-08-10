f64880477ace99b10ce498b7a374a50e03394abd
Arturo Peroni <arperoni@gmail.com>
2017-08-10 16:16:34 -0300

    Agrega script de carga de Departamentos.

---
+++ scripts/load_departments.py
@@ -0,0 +1,33 @@
+from georef.settings import BASE_DIR
+from geo_admin.models import Department, State
+import csv
+
+
+MESSAGES = {
+    'success': 'Los departamentos fueron cargados exitosamente.',
+    'error': 'Los departamentos no pudieron cargarse.'
+}
+
+
+def run():
+    state_ids = {state.code: state.id for state in State.objects.all()}
+    if state_ids:
+        departments = []
+        try:
+            file_path = BASE_DIR + '/data/departamentos.csv'
+            with open(file_path, newline='') as csv_file:
+                reader = csv.reader(csv_file)
+                next(reader) # Skips headers row.
+                for row in reader:
+                    dept_code, dept_name, state_code = row
+                    departments.append(Department(
+                        code=dept_code,
+                        name=dept_name,
+                        state_id=state_ids[state_code]
+                        ))
+            Department.objects.bulk_create(departments)
+            print(MESSAGES['success'])
+        except Exception as e:
+            print("{0}: {1}".format(MESSAGES['error'], e))
+    else:
+        print('Deben cargarse las provincias antes de los departamentos.')
