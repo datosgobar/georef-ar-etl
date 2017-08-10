fdc87a029a1faa98048e65d3f16c46f24c95bcfe
Marisol Cardozo <cardozo.marisolp@gmail.com>
2017-08-10 10:09:22 -0300

    Modifica los nombres de campos para el modelo de Calle

--- analytics/models.py
+++ analytics/models.py
@@ -3,17 +3,17 @@ from geo_admin.models import Locality, State
 
 
 class Search(models.Model):
-    text = models.CharField(max_length=250)
-    road_name = models.CharField(max_length=100, blank=True, null=True)
-    road_type = models.CharField(max_length=25, blank=True, null=True)
-    door_number = models.IntegerField(blank=True, null=True)
-    postal_code = models.CharField(max_length=8, blank=True, null=True)
-    locality = models.ForeignKey(Locality, blank=True, null=True)
-    state = models.ForeignKey(State, blank=True, null=True)
+    text = models.CharField(max_length=250, verbose_name='texto de búsqueda')
+    road_name = models.CharField(max_length=100, blank=True, null=True, verbose_name='nombre')
+    road_type = models.CharField(max_length=25, blank=True, null=True, verbose_name='tipo de camino')
+    door_number = models.IntegerField(blank=True, null=True, verbose_name='altura')
+    postal_code = models.CharField(max_length=8, blank=True, null=True, verbose_name='código postal')
+    locality = models.ForeignKey(Locality, blank=True, null=True, verbose_name='localidad')
+    state = models.ForeignKey(State, blank=True, null=True, verbose_name='provincia')
     timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name_plural = 'Searches'
+        verbose_name = 'búsqueda'
 
     def __str__(self):
         return self.text
--- geo_admin/models.py
+++ geo_admin/models.py
@@ -2,37 +2,47 @@ from django.db import models
 
 
 class State(models.Model):
-    code = models.CharField(max_length=2, blank=True, null=True)
-    name = models.CharField(max_length=100, blank=True, null=True)
+    code = models.CharField(max_length=2, blank=True, null=True, verbose_name='código')
+    name = models.CharField(max_length=100, blank=True, null=True, verbose_name='nombre')
+
+    class Meta:
+        verbose_name = 'provincia'
 
 
 class Department(models.Model):
-    code = models.CharField(max_length=5, blank=True, null=True)
-    name = models.CharField(max_length=100, blank=True, null=True)
-    state = models.ForeignKey(State, blank=True, null=True)
+    code = models.CharField(max_length=5, blank=True, null=True, verbose_name='código')
+    name = models.CharField(max_length=100, blank=True, null=True, verbose_name='nombre')
+    state = models.ForeignKey(State, blank=True, null=True, verbose_name='provincia')
+
+    class Meta:
+        verbose_name = 'departamento'
 
 
 class Locality(models.Model):
-    code = models.CharField(max_length=8, blank=True, null=True)
-    name = models.CharField(max_length=100, blank=True, null=True)
-    agglomerate = models.CharField(max_length=4, blank=True, null=True)
-    department = models.ForeignKey(Department, blank=True, null=True)
-    state = models.ForeignKey(State, blank=True, null=True)
+    code = models.CharField(max_length=8, blank=True, null=True, verbose_name='código')
+    name = models.CharField(max_length=100, blank=True, null=True, verbose_name='nombre')
+    agglomerate = models.CharField(max_length=4, blank=True, null=True,verbose_name='aglomerado')
+    department = models.ForeignKey(Department, blank=True, null=True, verbose_name='departamento')
+    state = models.ForeignKey(State, blank=True, null=True, verbose_name='provincia')
 
     class Meta:
-        verbose_name_plural = 'Localities'
+        verbose_name = 'localidad'
+        verbose_name_plural = 'localidades'
 
 
 class Road(models.Model):
-    code = models.CharField(max_length=13)
-    name = models.CharField(max_length=5)
-    road_type = models.CharField(max_length=25, blank=True, null=True)
-    start_left = models.IntegerField(blank=True, null=True)
-    start_right = models.IntegerField(blank=True, null=True)
-    end_left = models.IntegerField(blank=True, null=True)
-    end_right = models.IntegerField(blank=True, null=True)
-    geom = models.CharField(max_length=100, blank=True, null=True)
-    postal_code = models.CharField(max_length=8, blank=True, null=True)
-    locality = models.ForeignKey(Locality, blank=True, null=True)
-    state = models.ForeignKey(State, blank=True, null=True)
+    code = models.CharField(max_length=13, verbose_name='código')
+    name = models.CharField(max_length=5, verbose_name='nombre')
+    road_type = models.CharField(max_length=25, blank=True, null=True, verbose_name='tipo de camino')
+    start_left = models.IntegerField(blank=True, null=True, verbose_name='inicio izquierda')
+    start_right = models.IntegerField(blank=True, null=True, verbose_name='inicio derecha')
+    end_left = models.IntegerField(blank=True, null=True, verbose_name='fin izquierda')
+    end_right = models.IntegerField(blank=True, null=True, verbose_name='fin derecha')
+    geom = models.CharField(max_length=100, blank=True, null=True, verbose_name='geometría')
+    postal_code = models.CharField(max_length=8, blank=True, null=True, verbose_name='código postal')
+    locality = models.ForeignKey(Locality, blank=True, null=True, verbose_name='localidad')
+    state = models.ForeignKey(State, blank=True, null=True, verbose_name='provincia')
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'calle'
