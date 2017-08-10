d29f9ebb6b5a3283575e5fd9b184dad7688fdf1d
Arturo Peroni <arperoni@gmail.com>
2017-08-10 16:17:23 -0300

    Refactorización al script de carga de Provincias.

--- scripts/load_states.py
+++ scripts/load_states.py
@@ -5,14 +5,15 @@ from geo_admin.models import State
 
 MESSAGES = {
     'success': 'Las provincias fueron cargadas exitosamente.',
-    'error': 'Las provincias no pudieron cargarse'
+    'error': 'Las provincias no pudieron cargarse.'
 }
 
 
 def run():
     try:
         states = []
-        with open(BASE_DIR + '/data/provincias.csv', newline='') as csv_file:
+        file_path = BASE_DIR + '/data/provincias.csv'
+        with open(file_path, newline='') as csv_file:
             reader = csv.reader(csv_file)
             next(reader)
             for row in reader:
