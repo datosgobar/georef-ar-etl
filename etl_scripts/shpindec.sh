03fee8dd7ef27df6cd77bdc93a576cbcff83011e
Marisol Cardozo <cardozo.marisolp@gmail.com>
2017-08-14 12:29:36 -0300

    Refactoriza el script para consumir el shapefile de vías de circulación

--- etl_scripts/shpindec.sh
+++ etl_scripts/shpindec.sh
@@ -1,30 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# Servicio indec
-url='http://geoservicios.indec.gov.ar/geoserver/sig/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=sig:vias&outputFormat=SHAPE-ZIP'
-file='vias.zip'
+set -o errexit
+set -o nounset
 
-# Descargando el shafile
-wget --progress=dot -e dotbytes=1M -O vias.zip $url
+# Geoserver INDEC
+URL='http://geoservicios.indec.gov.ar/geoserver/sig/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=sig:vias&outputFormat=SHAPE-ZIP'
+FILE='vias.zip'
 
-# Descomprimiendo
-unzip vias.zip -d vias
+# Descarga la capa "Vías de circulación" en formato SHAPEFILE
+wget --progress=dot -e dotbytes=1M -O ${FILE} ${URL}
 
-# Cargar shape a PostgreSQL
-# Parametros de conexion
-host=localhost
-user=postgres
-dbname=test
-pass=postgres
+# Descomprime
+unzip ${FILE} -d vias
 
-# Importacion
-
+# Carga la capa Vías a PostgreSQL
 export SHAPE_ENCODING="LATIN1"
 
-ogr2ogr --config PGCLIENTENCODING LATIN1 -append -f "PostgreSQL" PG:"host=$host user=$user dbname=$dbname password=$pass" vias/vias.shp -nln indec_vias -nlt MULTILINESTRING -lco GEOMETRY_NAME=geom
+ogr2ogr -append -f "PostgreSQL" \
+    PG:"host=${POSTGRES_HOST} user=${POSTGRES_USER} dbname=${POSTGRES_DBNAME} password=${POSTGRES_PASSWORD}" \
+    vias/vias.shp -nln indec_vias -nlt MULTILINESTRING -lco GEOMETRY_NAME=geom
 
-# Genero un archivo log
+# Genera log de actividad
 echo "--------------------------------------------------------------------------- $(date)" >> indec.log
 ogrinfo -ro -so vias/vias.shp -al >> indec.log
-
 echo "Listo"
