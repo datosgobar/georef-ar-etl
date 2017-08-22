19f96e0c0d2e3fff5065c435e5b9414602911811
Marisol Cardozo <cardozo.marisolp@gmail.com>
2017-08-22 14:41:41 -0300

    Actualiza script etl vías

--- etl_scripts/etl_indec_vias.sh
+++ etl_scripts/etl_indec_vias.sh
@@ -3,30 +3,34 @@
 set -o errexit
 set -o nounset
 
-if [ -n ${POSTGRES_HOST} ] && [ -n ${POSTGRES_USER} ] && [ -n ${POSTGRES_DBNAME} ] && [ -n ${POSTGRES_PASSWORD} ] 
+# Valida que las variables esten decladaras
+if [ -n ${POSTGRES_HOST} ] && [ -n ${POSTGRES_USER} ] && [ -n ${POSTGRES_DBNAME} ] && [ -n ${POSTGRES_PASSWORD} ]
 then
+ # Valida la conexión a la base de datos
+ if ogrinfo "PG:host=${POSTGRES_HOST} dbname=${POSTGRES_DBNAME} user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}" | grep "successful"
+ then
+  # Geoserver INDEC
+  URL='http://geoservicios.indec.gov.ar/geoserver/sig/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=sig:vias&outputFormat=SHAPE-ZIP'
+  FILE='vias.zip'
 
- # Verifica la conexión
+  # Descarga la capa "Vías de circulación" en formato SHAPEFILE
+  wget --progress=dot -e dotbytes=1M -O ${FILE} ${URL}
 
- # Geoserver INDEC
- URL='http://geoservicios.indec.gov.ar/geoserver/sig/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=sig:vias&outputFormat=SHAPE-ZIP'
- FILE='vias.zip'
+  # Descomprime
+  unzip ${FILE} -d vias
 
- # Descarga la capa "Vías de circulación" en formato SHAPEFILE
- wget --progress=dot -e dotbytes=1M -O ${FILE} ${URL}
+  # Carga la capa Vías a PostgreSQL
+  export SHAPE_ENCODING="LATIN1"
 
- # Descomprime
- unzip ${FILE} -d vias
-
- # Carga la capa Vías a PostgreSQL
- export SHAPE_ENCODING="LATIN1"
-
- ogr2ogr -append -f "PostgreSQL" \
+  ogr2ogr -append -f "PostgreSQL" \
      PG:"host=${POSTGRES_HOST} user=${POSTGRES_USER} dbname=${POSTGRES_DBNAME} password=${POSTGRES_PASSWORD}" \
      vias/vias.shp -nln indec_vias -nlt MULTILINESTRING -lco GEOMETRY_NAME=geom
 
- # Genera log de actividad
- echo "--------------------------------------------------------------------------- $(date)" >> indec.log
- ogrinfo -ro -so vias/vias.shp -al >> indec.log
- echo "Listo"
+  # Genera log de actividad
+  echo "--------------------------------------------------------------------------- $(date)" >> indec.log
+  ogrinfo -ro -so vias/vias.shp -al >> indec.log
+  echo "Listo"
+  else
+   echo "La conexiónno tuvo �xito.";
+  fi
 fi
