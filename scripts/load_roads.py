a56a3b62608fb7658c08455ae5a6ca7ea5f4c90e
Arturo Peroni <arperoni@gmail.com>
2017-08-30 11:41:12 -0300

    Refactorización.

--- scripts/load_roads.py
+++ scripts/load_roads.py
@@ -13,23 +13,22 @@ def get_db_connection():
 
 
 def run_query():
+    query = """SELECT nomencla as code, \
+                    nombre as name, \
+                    tipo as road_type, \
+                    desdei as start_left, \
+                    desded as start_right, \
+                    hastai as end_left, \
+                    hastad as end_right, \
+                    geom, \
+                    codloc as locality \
+            FROM  indec_vias \
+            WHERE tipo <> 'OTRO';
+        """
     try:
-        query = """SELECT nomencla as code, \
-                          nombre as name, \
-                          tipo as road_type, \
-                          desdei as start_left, \
-                          desded as start_right, \
-                          hastai as end_left, \
-                          hastad as end_right, \
-                          geom, \
-                          codloc as locality \
-                    FROM  indec_vias \
-                    WHERE tipo <> 'OTRO';
-                """
         with get_db_connection().cursor() as cursor:
             cursor.execute(query)
             streets = cursor.fetchall()
-            cursor.close()
         return streets
     except psycopg2.DatabaseError as e:
         print(e)
@@ -44,12 +43,11 @@ def run():
         streets = run_query()
         print('-- Procesando vías --')
         for row in streets:
-            code, name, road_type, start_left, start_right, end_left, \
-            end_right, geom, codloc = row
-            if codloc in localities and localities[codloc][1]:
-                # codloc = '02000010' if '02000000' < codloc < '03000000' else codloc
-                locality_id = localities[codloc][0]
-                state_id = localities[codloc][1]
+            (code, name, road_type, start_left, start_right,
+            end_left, end_right, geom, locality) = row
+            if locality in localities:
+                locality_id = localities[locality][0]
+                state_id = localities[locality][1]
                 roads.append(Road(
                     code=code,
                     name=name,
@@ -59,17 +57,17 @@ def run():
                     end_left=end_left,
                     end_right=end_right,
                     geom=geom,
-                    locality_id=locality_id if locality_id else None,
-                    state_id=state_id if state_id else None,
+                    locality_id=locality_id,
+                    state_id=state_id
                 ))
             else:
-                failed_rows.append({'nombre': name, 'codloc': codloc, 'nomencla': code})
+                failed_rows.append({'nombre': name, 'codloc': locality, 'nomencla': code})
         print('-- Insertando vías --')
         Road.objects.bulk_create(roads)
         if failed_rows:
             print('-- Generando log de errores --')
             with open('failed_roads.json', 'w') as f:
-                json.dump(failed_rows, f, indent=4)
+                json.dump(failed_rows, f, indent=2)
         print('-- Proceso completo --')
     except Exception as e:
         print(e)
