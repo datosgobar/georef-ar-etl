1fb4568e442ac545cfed26071bbf92f6bdfbf3d2
Marisol Cardozo <cardozo.marisolp@gmail.com>
2018-01-30 10:21:08 -0300

    Agrega al script de ETL la entidad de BARHA

--- etl_scripts/etl_ign_entidades.sh
+++ etl_scripts/etl_ign_entidades.sh
@@ -22,7 +22,6 @@ then
     declare URL="${URL_PORTAL}${FILE}"
 
     wget --progress=dot -e dotbytes=1M -O ${FILE} ${URL}
-
     unzip ${FILE} -d ${i}
 
     ogr2ogr -overwrite -progress -f "PostgreSQL" \
@@ -32,6 +31,22 @@ then
     echo "--------------------------------------------------------------------------- $(date)" >> ign.log
     ogrinfo -ro -so "${i}/${i}.shp" -al >> ign.log
   done
+
+  declare URL_SHP_BARHA='http://wms.ign.gob.ar/geoserver/idera/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=idera:bahra&outputFormat=SHAPE-ZIP'
+  declare FILE_BARHA='barha.zip'
+
+  wget --progress=dot -e dotbytes=1M -O ${FILE_BARHA} ${URL_SHP_BARHA}
+  unzip ${FILE_BARHA} -d bahra
+
+  export SHAPE_ENCODING="LATIN1"
+
+  ogr2ogr -overwrite -progress -f "PostgreSQL" \
+  PG:"host=${POSTGRES_HOST} user=${POSTGRES_USER} dbname=${POSTGRES_DBNAME} password=${POSTGRES_PASSWORD}" \
+  bahra/bahra.shp -nln ign_bahra -nlt POINT -lco GEOMETRY_NAME=geom
+
+  echo "--------------------------------------------------------------------------- $(date)" >> ign.log
+  ogrinfo -ro -so "${i}/${i}.shp" -al >> ign.log
+
   echo "Terminado!"
   fi
 fi
