f8c5ab34e6c12e3815ce49402d793b66ec178b8e
Marisol Cardozo <cardozo.marisolp@gmail.com>
2018-02-08 16:21:19 -0300

    Agrega el proceso de mapeo por cada índice de Elasticsearch

--- scripts/index_entities.py
+++ scripts/index_entities.py
@@ -20,6 +20,20 @@ def index_states(es):
         print('-- Ya existe el índice de Provincias.')
         return
     print('-- Creando índice de Provincias.')
+
+    mapping = {
+        'provincia': {
+            'properties': {
+                'id': {'type': 'text'},
+                'name': {'type': 'text'},
+                'geometry': {'type': 'geo_shape'}
+            }
+        }
+    }
+    es.indices.create(index='provincias')
+    es.indices.put_mapping(index='provincias', doc_type='provincia',
+                           body=mapping)
+
     data = []
     for state in State.objects.all():
         data.append({'index': {'_id': state.id}})
@@ -27,13 +41,13 @@ def index_states(es):
             'id': state.code,
             'nombre': state.name,
             'geometry': {
-                'type': state.geom.geom_type,
-                'coordinates': state.geom.coords,
-                'geometry_name': 'geom'
+                'type': 'polygon',
+                'coordinates': state.geom.coords
             }
         })
     es.bulk(index='provincias', doc_type='provincia', body=data, refresh=True,
-            request_timeout=120)
+            request_timeout=320)
+
     print('-- Se creó el índice de Provincias exitosamente.')
 
 
@@ -42,17 +56,38 @@ def index_departments(es):
         print('-- Ya existe el índice de Departamentos.')
         return
     print('-- Creando índice de Departamentos.')
+
+    mapping = {
+        'departamento': {
+            'properties': {
+                'id': {'type': 'text'},
+                'name': {'type': 'text'},
+                'geometry': {'type': 'geo_shape'},
+                'provincia': {
+                    'type': 'object',
+                    'properties': {
+                        'id': {'type': 'text'},
+                        'name': {'type': 'text'},
+                    }
+                }
+            }
+        }
+    }
+    es.indices.create(index='departamentos')
+    es.indices.put_mapping(index='departamentos', doc_type='departamento',
+                           body=mapping)
+
     data = []
     states = {state.id: (state.code, state.name) for state in State.objects.all()}
     for dept in Department.objects.all():
         geometry = {}
         if dept.geom is not None:
             geometry = {
-                'type': dept.geom.geom_type,
+                'type': 'polygon',
                 'coordinates': dept.geom.coords,
-                'geometry_name': 'geom'
             }
-        document = {
+        data.append({'index': {'_id': dept.id}})
+        data.append({
             'id': dept.code,
             'nombre': dept.name,
             'geometry': geometry,
@@ -60,11 +95,9 @@ def index_departments(es):
                 'id': states[dept.state_id][0],
                 'nombre': states[dept.state_id][1]
             }
-        }
-        data.append({'index': {'_id': dept.id}})
-        data.append(document)
+        })
     es.bulk(index='departamentos', doc_type='departamento', body=data,
-            refresh=True, request_timeout=120)
+            refresh=True, request_timeout=320)
     print('-- Se creó el índice de Departamentos exitosamente.')
 
 
@@ -73,11 +106,33 @@ def index_municipalities(es):
         print('-- Ya existe el índice de Municipios.')
         return
     print('-- Creando índice de Municipios.')
+
+    mapping = {
+        'municipio': {
+            'properties': {
+                'id': {'type': 'text'},
+                'name': {'type': 'text'},
+                'geometry': {'type': 'geo_shape'},
+                'provincia': {
+                    'type': 'object',
+                    'properties': {
+                        'id': {'type': 'text'},
+                        'name': {'type': 'text'},
+                    }
+                }
+            }
+        }
+    }
+    es.indices.create(index='municipios')
+    es.indices.put_mapping(index='municipios', doc_type='municipio',
+                           body=mapping)
+
     data = []
     states = {state.id: (state.code, state.name) for state in
               State.objects.all()}
     for mun in Municipality.objects.all():
-        document = {
+        data.append({'index': {'_id': mun.id}})
+        data.append({
             'id': mun.code,
             'nombre': mun.name,
             'provincia': {
@@ -85,15 +140,13 @@ def index_municipalities(es):
                 'nombre': states[mun.state_id][1]
             },
             'geometry': {
-                'type': mun.geom.geom_type,
+                'type': 'polygon',
                 'coordinates': mun.geom.coords,
-                'geometry_name': 'geom'
             }
-        }
-        data.append({'index': {'_id': mun.id}})
-        data.append(document)
-    es.bulk(index='municipios', doc_type='municipios', body=data,
-            refresh=True, request_timeout=120)
+        })
+
+    es.bulk(index='municipios', doc_type='municipio', body=data,
+            refresh=True, request_timeout=320)
     print('-- Se creó el índice de Municipios exitosamente.')
 
 
@@ -106,7 +159,8 @@ def index_localities(es):
     states = {state.id: (state.code, state.name) for state in State.objects.all()}
     departments = {dept.id: (dept.code, dept.name) for dept in Department.objects.all()}
     for locality in Locality.objects.all():
-        document = {
+        data.append({'index': {'_id': locality.id}})
+        data.append({
             'id': locality.code,
             'nombre': locality.name,
             'departamento': {
@@ -117,9 +171,7 @@ def index_localities(es):
                 'id': states[locality.state_id][0],
                 'nombre': states[locality.state_id][1]
             }
-        }
-        data.append({'index': {'_id': locality.id}})
-        data.append(document)
+        })
     es.bulk(index='localidades', doc_type='localidad', body=data, refresh=True,
             request_timeout=120)
     print('-- Se creó el índice de Localidades exitosamente.')
@@ -130,6 +182,34 @@ def index_settlements(es):
         print('-- Ya existe el índice de BAHRA.')
         return
     print('-- Creando índice de Asentamientos.')
+
+    mapping = {
+        'asentamiento': {
+            'properties': {
+                'id': {'type': 'text'},
+                'nombre': {'type': 'text'},
+                'tipo': {'type': 'text'},
+                'departamento': {
+                    'type': 'object',
+                    'properties': {
+                        'id': {'type': 'text'},
+                        'name': {'type': 'text'},
+                    }
+                },
+                'provincia': {
+                    'type': 'object',
+                    'properties': {
+                        'id': {'type': 'text'},
+                        'name': {'type': 'text'},
+                    }
+                },
+                'geometry': {'type': 'geo_shape'},
+            }
+        }
+    }
+    es.indices.create(index='bahra')
+    es.indices.put_mapping(index='bahra', doc_type='asentamiento', body=mapping)
+
     barha_types = {
         'E': 'Entidad (E)',
         'LC': 'Componente de localidad compuesta (LC)',
@@ -139,7 +219,8 @@ def index_settlements(es):
     states = {state.id: (state.code, state.name) for state in State.objects.all()}
     departments = {dept.id: (dept.code, dept.name) for dept in Department.objects.all()}
     for settlement in Settlement.objects.all():
-        document = {
+        data.append({'index': {'_id': settlement.id}})
+        data.append({
             'id': settlement.code,
             'nombre': settlement.name,
             'tipo': barha_types[settlement.bahra_type],
@@ -152,13 +233,10 @@ def index_settlements(es):
                 'nombre': states[settlement.state_id][1]
             },
             'geometry': {
-                'type': settlement.geom.geom_type,
-                'coordinates': settlement.geom.coords,
-                'geometry_name': 'geom'
+                'type': 'multipoint',
+                'coordinates': settlement.geom.coords
             }
-        }
-        data.append({'index': {'_id': settlement.id}})
-        data.append(document)
+        })
     es.bulk(index='bahra', doc_type='asentamiento', body=data, refresh=True,
-            request_timeout=120)
+            request_timeout=320)
     print('-- Se creó el índice de Asentamientos exitosamente.')
