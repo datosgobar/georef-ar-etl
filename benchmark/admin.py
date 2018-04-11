87da99283e39f9f5f465df34dcf159156842fb6a
Marisol Cardozo <cardozo.marisolp@gmail.com>
2018-04-11 13:06:29 -0300

    Remueve el módulo Benchmark

--- benchmark/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from benchmark.models import *
-
-
-admin.site.register(Address)
-admin.site.register(AddressType)
-admin.site.register(City)
-admin.site.register(State)
