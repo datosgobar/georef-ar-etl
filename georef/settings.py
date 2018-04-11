9c2dbcaa98a42c047cd3f65fc879386ff96df770
Marisol Cardozo <cardozo.marisolp@gmail.com>
2018-04-11 13:41:57 -0300

    Modifica el archivo de configuraciones de Django.

--- georef/settings.py
+++ georef/settings.py
@@ -18,144 +18,30 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
-if os.environ.get('DJANGO_ENVIRONMENT', 'dev') == 'dev':
-    SECRET_KEY = 'k3jneu6o!cb$qnqm8=t2dxvd@-t-)y-*43=ul42a@^l48jf#h_'
-    DEBUG = True
-else:
-    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-    DEBUG = False
+DEBUG = os.environ.get('DEBUG')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django_extensions',
-    'analytics',
-    'benchmark',
     'geo_admin',
 ]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'georef.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'georef.wsgi.application'
-
-
-API_URL = os.environ.get('GEOREF_API_URL')
-KONG_URL = os.environ.get('KONG_URL')
-OSM_API_URL = 'http://nominatim.openstreetmap.org/'
-HERE = {
-    'API_URL': 'https://geocoder.cit.api.here.com/6.2/geocode.json',
-    'APP_CODE': os.environ.get('HERE_APP_CODE'),
-    'APP_ID': os.environ.get('HERE_APP_ID')
-}
-
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 GEOREF = {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': os.environ.get('GEOREF_DB_NAME'),
-        'USER': os.environ.get('GEOREF_DB_USER'),
-        'PASSWORD': os.environ.get('GEOREF_DB_PASS'),
-        'HOST': os.environ.get('GEOREF_DB_HOST'),
-        'PORT': '5432',
-    }
-
-SQLITE = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-
-KONG = {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kong',
-        'USER': os.environ.get('KONG_DB_USER'),
-        'PASSWORD': os.environ.get('KONG_DB_PASS'),
-        'HOST': os.environ.get('KONG_HOST'),
-        'PORT': '5432',
-    }
-
-DATABASES = {
-    'default': SQLITE if os.environ.get('DJANGO_ENVIRONMENT', 'dev') == 'dev'
-        else GEOREF,
-    'kong': KONG
+    'ENGINE': 'django.contrib.gis.db.backends.postgis',
+    'NAME': os.environ.get('GEOREF_DB_NAME'),
+    'USER': os.environ.get('GEOREF_DB_USER'),
+    'PASSWORD': os.environ.get('GEOREF_DB_PASS'),
+    'HOST': os.environ.get('GEOREF_DB_HOST'),
+    'PORT': '5432',
 }
 
-
-LOGIN_URL = '/login'
-LOGIN_REDIRECT_URL = '/'
-
-# Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.10/topics/i18n/
-
-LANGUAGE_CODE = 'es-ar'
-
-TIME_ZONE = 'America/Buenos_Aires'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+DATABASES = {'default': GEOREF}
