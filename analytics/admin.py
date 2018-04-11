e27e406fbe4911a5512c31972a405cee30d9c2c2
Marisol Cardozo <cardozo.marisolp@gmail.com>
2018-04-11 13:07:02 -0300

    Remueve el módulo Analytics

--- analytics/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from analytics.models import Search
-
-
-admin.site.register(Search)
--- analytics/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-
-urlpatterns = [
-    url(r'^save_address_search', views.save_address_search, name='save_address_search'),
-]
