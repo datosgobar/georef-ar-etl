a928287cb5ed697e1e45c76b189edd404dcaaca1
Marisol Cardozo <cardozo.marisolp@gmail.com>
2018-10-10 12:28:37 -0300

    Actualiza código de asentamientos a partir del cambio de código de departamentos.

--- etl_scripts/ign_entities_patch.sql
+++ etl_scripts/ign_entities_patch.sql
@@ -10,15 +10,16 @@ ALTER TABLE IF EXISTS ign_municipios ALTER COLUMN geom TYPE geometry(MultiPolygo
 -- DEPARTAMENTOS
 DELETE FROM ign_departamentos WHERE ogc_fid = 530; -- duplicado
 
-UPDATE ign_departamentos SET in1 = '54084' WHERE in1 = '55084';
-UPDATE ign_departamentos SET in1 = '06217' WHERE in1 = '06218';
-UPDATE ign_departamentos SET in1 = '94008' WHERE in1 = '94007';
-UPDATE ign_departamentos SET in1 = '94015' WHERE in1 = '94014';
-UPDATE ign_departamentos SET in1 = '94011' WHERE fna = 'Departamento Río Grande' AND nam = 'Tolhuin';
+UPDATE ign_departamentos SET in1 = '54084' WHERE in1 = '55084'; -- Error de tipeo
+UPDATE ign_departamentos SET in1 = '06217' WHERE in1 = '06218'; -- Chascomús
+UPDATE ign_departamentos SET in1 = '94008' WHERE in1 = '94007'; -- Río Grande *
+UPDATE ign_departamentos SET in1 = '94015' WHERE in1 = '94014'; -- Usuhaia **
+UPDATE ign_departamentos SET in1 = '94011' WHERE fna = 'Departamento Río Grande' AND nam = 'Tolhuin'; -- Nulo
 
 -- MUNICIPIOS
 DELETE FROM ign_municipios WHERE in1 ISNULL ;
 
+-- Error de asignación de código
 UPDATE ign_municipios SET in1 = '540287' WHERE in1 = '550287';
 UPDATE ign_municipios SET in1 = '540343' WHERE in1 = '550343';
 UPDATE ign_municipios SET in1 = '820277' WHERE in1 = '800277';
@@ -46,3 +47,17 @@ UPDATE ign_bahra SET cod_depto = '084' WHERE nom_depto = 'COMUNA 12';
 UPDATE ign_bahra SET cod_depto = '091' WHERE nom_depto = 'COMUNA 13';
 UPDATE ign_bahra SET cod_depto = '098' WHERE nom_depto = 'COMUNA 14';
 UPDATE ign_bahra SET cod_depto = '105' WHERE nom_depto = 'COMUNA 15';
+
+-- * Actualiza códigos para los asentamientos del departamento de Río Grande
+UPDATE ign_bahra SET cod_bahra = '94008000010' WHERE cod_bahra = '94007000010';
+UPDATE ign_bahra SET cod_bahra = '94008000013' WHERE cod_bahra = '94007000013';
+UPDATE ign_bahra SET cod_bahra = '94008000073' WHERE cod_bahra = '94007000073';
+UPDATE ign_bahra SET cod_bahra = '94008000146' WHERE cod_bahra = '94007000146';
+UPDATE ign_bahra SET cod_bahra = '94008010000' WHERE cod_bahra = '94007010000';
+UPDATE ign_bahra SET cod_bahra = '94008020000' WHERE cod_bahra = '94007020000';
+UPDATE ign_bahra SET cod_depto = '008' WHERE cod_prov = '94' AND cod_depto = '007';
+
+-- ** Actualiza códigos para los asentamientos del departamento de Usuhaia
+UPDATE ign_bahra SET cod_bahra = '94015010000' WHERE cod_bahra = '94014010000';
+UPDATE ign_bahra SET cod_bahra = '94015020000' WHERE cod_bahra = '94014020000';
+UPDATE ign_bahra SET cod_depto = '015' WHERE cod_prov = '94' AND cod_depto = '014';
