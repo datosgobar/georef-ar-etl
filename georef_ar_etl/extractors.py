2227e1fd70f2b2724f4a015d8637eb0e89afde28
federicotdn <federicotedin.ar@gmail.com>
2019-05-08 12:43:46 -0300

    Agrega hash MD5 al reporte luego de descarga de archivos.

--- georef_ar_etl/extractors.py
+++ georef_ar_etl/extractors.py
@@ -1,3 +1,4 @@
+import hashlib
 import requests
 from .process import Step, ProcessException
 
@@ -15,15 +16,21 @@ class DownloadURLStep(Step):
             return self._filename
 
         ctx.report.info('Descargando: %s', self._url)
+        chunk_size = ctx.config.getint('etl', 'chunk_size')
+
         with requests.get(self._url, stream=True, params=self._params) as req:
             if req.status_code != 200:
                 raise ProcessException(
                     'La petición HTTP retornó código {}.'.format(
                         req.status_code))
 
-            chunk_size = ctx.config.getint('etl', 'chunk_size')
+            md5 = hashlib.md5()
             with ctx.fs.open(self._filename, 'wb') as f:
                 for chunk in req.iter_content(chunk_size=chunk_size):
                     f.write(chunk)
+                    md5.update(chunk)
+
+            ctx.report.info('Archivo descargado. Hash MD5: {}'.format(
+                md5.hexdigest()))
 
         return self._filename
